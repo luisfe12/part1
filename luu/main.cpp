817466d68f20b1ab13248cfd9f0d67d3d7186171
cs-unsa16 <luisfevf12@gmail.com>
2018-08-24 08:24:12 -0500

    ha vuelto

---
+++ luu/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    int p;
+    cin >> r;
+    cin >> p;
+    cout << "Hello world!" << endl;
+    cout << r+p << endl;
+    return 0;
+}
