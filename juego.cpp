7f0c8f652a17e4d8c32dfe738ae003a46828e469
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2018-10-01 13:56:55 -0500

    Add files via upload

---
+++ juego.cpp
@@ -0,0 +1,27 @@
+#include<SFML/Graphics.hpp>
+#include<iostream>
+using namespace std;
+using namespace sf;
+
+int main(){
+
+bool juego = false;
+
+RenderWindow *ventana1;
+
+ventana1=new RenderWindow(VideoMode(800,600),"tu amam");
+
+while(!juego){
+
+    ventana1->display();
+
+
+}
+
+
+
+
+
+
+
+}
