357331a479fad337cc163b4255fb8437b7fa3c55
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2018-10-04 21:09:01 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 
 using namespace std;
+bool perfect(int a){
+int cont=0;
+for(int i=1;i<a;i++){
+    if(a%i==0){
+    cont+=i;
+    }
+  }
+if(cont==a)
+    return true;
+return false;
+}
+
+
+
+void perfecto(int *a,int tam){
+int cont=0;
+for(int i=1;i<tam;i++){
+    if(perfect(*(a+i))){
+    cout<< *(a+i)<<" ";
+    }}}
+/*}
+if(tam==cont){
+    return cont;
+}
+else{return cont;}
+}*/
+
 
 int main()
 {
-    int a[2][2],b[2][2],c[2][2];
+   int a[6]={1,2,3,4,27,28};
 
-
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            cin>>a[i][j];
-        }
-
-    }
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            cin>>b[i][j];
-        }
-    }
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            c[i][j]=0;
-            for(int k=0;k<2;k++){
-                c[i][j]=c[i][j]+(a[i][k]*b[k][j]);
-            }
-        }
-
-    }
-    for(int i=0;i<2;i++){
-        for(int j=0;j<2;j++){
-            cout<<c[i][j]<<endl;
-        }
-    }
-
+   perfecto(a,6);
+    return 0;
 }
