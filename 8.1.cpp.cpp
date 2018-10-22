2b6bdf1a1a4120a878cdaec36eb144b0cca9e08c
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2018-10-21 21:37:09 -0500

    Add files via upload

---
+++ 8.1.cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   Answer each of the following:
+    a) A pointer is a variablethat contains as its value the_________ of another variable.
+   b) A pointer should be initialized to_______ or_______ .
+   c) The only integer that can be assigned directly to a pointer is_______ .
+
+    a) address.
+    b) nullptr, an address.
+    c) 0.
+}
