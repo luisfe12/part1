2c5ca78d2dd2f0761ccde41601dec51f6907b61c
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2018-10-21 21:53:04 -0500

    Add files via upload

---
+++ 8.10.cpp.cpp
@@ -0,0 +1,15 @@
+(a) Escribir el encabezado para la funcion cero, que reciba un parametro tipo arreglo integrado de enteros long llamado enteroGrandes, y que no devuelva un valor.
+
+
+        int cero( long enteroGrandes[] )
+
+(b) Escriba el prototipo para la funcion parte (a).
+
+        int cero( long [] )
+(c) Escriba el encabezado para la funcion sumar1YSumar, que reciba un parametro tipo arreglo integrado entero unoDemasiadoChico y devuelva un entero.
+
+        int sumar1YSumar( int unoDemasiadoChico[] )
+
+(d) Escribir el prototipo para la funcion descrita en la parte (c)
+
+        int sumar1Ysumar( int [] )
