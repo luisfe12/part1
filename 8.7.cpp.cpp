c86a2de13353184fa4f2baae183c137ef85eb870
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2018-10-21 21:50:27 -0500

    Add files via upload

---
+++ 8.7.cpp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    (True or False) State whether the following are true or false. If false, explain why.
+    a) Two pointers that point to different built-in arrays cannot be compared meaningfully.
+
+    Verdadero. Debido a que las comparaciones en las que se utilizan operadores relacionales no tienen significado, a menos que los apuntadores apunten a miembros del mismo arreglo integrado.
+
+    b) Becausethe name of a built-inarray is implicitly convertible to apointer to the first elementof the built-in array, built-in array names can be manipulated in the same manner as pointers.
+
+    Falso. La modificacion no se puede hacer a la matriz nombrada de modo que se pueda apuntar a la ubicacion diferente en la memoria. Esto se debe a que el nombre de la matri es basicament el puntero a los elementos iniciales de la matriz. La manipulacion en los nombre de la matriz se realiza de manera similar a como se hace en el puntero.
+}
