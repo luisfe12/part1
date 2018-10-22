c3bca4c43c1f80a94f185da0ef80260c5e2e2974
luisfe12 <42527787+luisfe12@users.noreply.github.com>
2018-10-21 21:47:38 -0500

    Add files via upload

---
+++ 8.5.cpp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Perform the task specified by each of the following statements:
+        a) Writethefunctionheaderforafunctioncalled exchange thattakestwopointerstodouble-precision,floating-pointnumbers x andy asparametersanddoesnotreturnavalue.
+
+        void exchange( double *x, double *y )
+
+        b) Write the function prototype for the function in part (a).
+
+         void exchange( double *, double * );
+
+        c) Write two statements that each initialize the built-in array of chars named vowel with the string of vowels, "AEIOU".
+
+        char vowel[] = "AEIOU";
+        char vowel[] = { 'A', 'E', 'I', 'O', 'U', '\0' };
+
+}
